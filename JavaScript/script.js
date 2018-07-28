c4b7391dca2f9a9a2804feb565942e8aab22d0c0
Tejas Shinde <shindetejas2011@gmail.com>
2018-07-28 16:27:56 +0530

    Made 3 pages responsive

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
 	let navigation=$('nav');
 	let menu=$('#hamburger');
+	let backToStart = $('.return_to_start');
 
 	menu.click(function(){
 		navigation.toggleClass('slide-right');
@@ -10,4 +11,26 @@ $(document).ready(function(){
 	$('li>a, main, #close img').click(function(){
 		navigation.addClass('slide-right');
 	});
+
+	window.addEventListener('scroll', function(){
+		const yLocation = window.scrollY;
+		if (yLocation >= 530){
+			$('.percentage-1').addClass('animate-3');
+			$('.percentage-2').addClass('animate-4');
+			$('.percentage-3').addClass('animate-5');
+		}
+
+		if (yLocation >= 480){
+			backToStart.fadeIn("slow");
+		} else {
+			backToStart.fadeOut("slow");
+		}
+	});
+
+	backToStart.click(function(evt){
+        $('body').scrollTop(0);
+    });
+
 });
+
+
